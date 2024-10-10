1a0d36b218291bb5d48ce79c834d14f9b83a8b05
lenok-ar <leno4ka279a@gmail.com>
2024-10-10 23:48:37 +0700

    Add files via upload

---
+++ Lab_Four.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <time.h>
+#include <stdlib.h>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    srand(time(NULL));
+
+    int const N = 6;
+    int const M = 6;
+    int sum, count;
+ 
+    int A[N][M];
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < M; ++j) {
+            A[i][j] = rand() % 20 - 10;
+        }
+    }
+    
+    cout << "Вывод матрицы: " << endl;
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < M; ++j) {
+            cout << A[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+
+    for (int i = 0; i < N; ++i) {
+        sum = 0;
+        for (int j = 0; j < M; ++j) {
+            if (A[i][j] < 0) {
+                sum += A[i][j];
+            }
+        }
+        cout << "Сумма отрицательных элементов в " << ++A[i] << " строке = " << sum << endl;
+    }
+    cout << endl;
+
+    for (size_t i = 0; i < N; ++i) {
+        count = 0;
+        for (size_t j = 0; j < M; ++j) {
+            if (A[i][j] < 0) {
+                ++count;
+            }
+        }
+        cout << "Число отрицательных элементов в " << ++A[i] << " строке = " << count << endl;
+    }
+}
